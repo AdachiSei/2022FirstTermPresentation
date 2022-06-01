47d47ccaa31566207b66160d6905721c4d85b961
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-06-01 12:20:52 +0900

    ゲームマネ＝ジャー制作中

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// ゲームマネージャー
+/// 対戦ゲームなので勝敗の処理はここで行う
+/// 貰える金額も決められる
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    /// <summary>何ポイント先取で勝ちか</summary>
+    [SerializeField]
+    [Header("何ポイント先取で勝ちか")]
+    [Range(1,100)]
+    int _points = 3;
+
+    /// <summary>相手より長く回って勝利した時に貰えるポイント</summary>
+    [SerializeField]
+    [Header("相手より長く回った時に貰えるポイント")]
+    [Range(0,100)]
+    int _spinFinishPoints = 1;
+
+    /// <summary>相手を場外に追い出したときに貰えるポイント</summary>
+    [SerializeField]
+    [Header("相手を場外に追い出した時に貰えるポイント")]
+    [Range(0, 100)]
+    int _overFinishPoints = 1;
+
+    /// <summary>相手を破壊した時に貰えるポイント(実装未定)</summary>
+    [SerializeField]
+    [Header("相手を破壊した時に貰えるポイント(実装未定)")]
+    [Range(0, 100)]
+    int _burstFinishPoints = 2;
+
+    /// <summary>試合後に必ず貰える金額</summary>
+    [SerializeField]
+    [Header("試合後に必ず貰える金額")]
+    int _money;
+
+    /// <summary>勝利後に貰える金額のボーナス</summary>
+    [SerializeField]
+    [Header("勝利時にもらえる金額のボーナス")]
+    float _victoryMoney;
+
+    /// <summary>相手より長く回る度に貰える金額のボーナス</summary>
+    [SerializeField]
+    [Header("相手より長く回る度に貰える金額のボーナス")]
+    float _spinFinishBonus;
+
+    /// <summary>相手を場外に追い出す度に貰える金額のボーナス</summary>
+    [SerializeField]
+    [Header("相手を場外に追い出す度に貰える金額のボーナス")]
+    float _overFinishBonus;
+
+    /// <summary>相手を破壊する度に貰える金額のボーナス(実装未定)</summary>
+    [SerializeField]
+    [Header("相手を破壊する度に貰える金額のボーナス(実装未定)")]
+    float _burstFinishBonus;
+
+    /// <summary>Player1のゲームオブジェクト</summary>
+    GameObject _firstPlayer;
+    /// <summary>Player2のゲームオブジェクト</summary>
+    GameObject _secondPlayer;
+
+    /// <summary>Player1が持っている勝利ポイント</summary>
+    int _firstPlayerPoint;
+    /// <summary>Player2が持っている勝利ポイント</summary>
+    int _secondPlayerPoint;
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
